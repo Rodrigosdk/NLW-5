8e9f6381a5d8697b540d3dd3001ee4213199534f
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-04 22:43:16 -0300

    Validate exist user

--- src/services/SettingsServices.ts
+++ src/services/SettingsServices.ts
@@ -10,13 +10,22 @@ class SettingsServices {
   async create({ chat, username }: ISettingsCreate) {
     const settingRepository = getCustomRepository(SettingRepository);
 
+    const userAlreadyExists = await settingRepository.findOne({
+      username,
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
     const settings = settingRepository.create({
       chat,
       username,
     });
 
-    await settingRepository.save(settings); 
-    return settings
+    await settingRepository.save(settings);
+
+    return settings;
   }
 }
 
