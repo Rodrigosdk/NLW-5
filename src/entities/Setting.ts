2ab70b3eebaeefb69d9863c23bcb1e6df3ddc248
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-19 12:33:56 -0300

    Create entities user

--- src/entities/Setting.ts
+++ src/entities/Setting.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryColumn} from 'typeorm'
+import {Entity, Column, UpdateDateColumn, PrimaryColumn} from 'typeorm'
 import { v4 as uuid } from 'uuid'
 
 @Entity('settings')
---
+++ src/entities/User.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, UpdateDateColumn, PrimaryColumn } from 'typeorm'
+import { v4 as uuid } from 'uuid'
+
+@Entity("user")
+export class User {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    email: string;
+
+    @UpdateDateColumn()
+    created_at: Date
+
+    constructor (){
+        if (!this.id){
+            this.id = uuid()
+        }
+    }
+}
