2637f97b96c457cb6a1479a80925d50fd4ea2649
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-19 13:45:38 -0300

    Create route users

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,9 +1,14 @@
-import {SettingsController} from '../controller/SettingController';
+import { SettingsController } from '../controller/SettingController';
+import { UserController } from '../controller/UserController'
+
 
 export class Routes {
     private settingsController: SettingsController = new SettingsController();
+    private userController: UserController = new UserController();
     
-    public router(app){
-        app.post('/settings', this.settingsController.create)
+    public router(app): void {
+        app.post('/settings', this.settingsController.create);
+        app.post('/users', this.userController.create);
+
     }
 }
