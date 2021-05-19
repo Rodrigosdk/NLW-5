4194d47547ff7ae586ae2da96a1582336f1612ec
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-18 21:28:30 -0300

    server refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 
-import {routers} from "./router";
+import { routers } from "./router";
 import './database';
 
-const app = express();
+class App {
+    public app: express.Application;
 
-app.use(express.json());
-app.use(routers)
+    constructor() {
+        this.app = express();
+        this.middlewares();
+        this.routes();
 
-app.listen(3333, () => console.log('server online'));
+    }
+    private middlewares(): void {
+        this.app.use(express.json());
+    }
+    private routes(): void {
+        this.app.use(routers);
+    }
+}
+
+export default new App().app
