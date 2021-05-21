8658e2285a10fa6c64fd961991d45c052f0f4843
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-21 13:19:51 -0300

    Add types to app

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,13 +1,15 @@
+import { Application } from 'express';
+
 import { SettingsController } from '../controller/SettingController';
 import { UserController } from '../controller/UserController';
 import { MenssageController } from '../controller/MenssageController';
 
-export class Routes {
+export class A {
     private settingsController: SettingsController = new SettingsController();
     private userController: UserController = new UserController();
     private menssageController: MenssageController = new MenssageController();
 
-    public router(app): void {
+    public init(app: Application): void {
         app.post('/settings', this.settingsController.create);
         app.post('/users', this.userController.create);
         app.post('/menssages', this.menssageController.create);
